adc728e077e1ec98d8bd8955db91879ecdeef600
logonrm <logonrm@fiap.com.br>
2018-04-13 22:40:50 -0300

    vergonha

---
+++ NACDado/app/src/main/java/lucasquincosestoledo/com/br/nacdado/Dado_Activity.kt
@@ -0,0 +1,12 @@
+package lucasquincosestoledo.com.br.nacdado
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Dado_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dado_)
+    }
+}
---
+++ app/src/main/java/lucasquincosestoledo/com/br/nacdado/Dado_Activity.kt
@@ -0,0 +1,13 @@
+package lucasquincosestoledo.com.br.nacdado
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Dado_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dado_)
+    }
+}
+//me desculpa
